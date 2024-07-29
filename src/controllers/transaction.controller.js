14d980788d8621dff72fc0c871937e96d9ff17cd
Md-Nur <md.nurealamsiddiquee2004@gmail.com>
2024-07-30 02:56:55 +0600

    cash out api done

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -66,7 +66,6 @@ const sendMoney = asyncHandler(async (req, res) => {
       .json(
         new ApiError(500, "You don't have sufficient balance to transfer!")
       );
-      
   }
 
   // Add a tranction in both req and res user
@@ -103,4 +102,95 @@ const sendMoney = asyncHandler(async (req, res) => {
     .send(new ApiResponse(201, tranction, "Send money successfully"));
 });
 
-export { sendMoney };
+const cashOut = asyncHandler(async (req, res) => {
+  // get transaction from frontend
+  let { reqPhone, resPhone, pin, amount } = req.body;
+
+  // validation - not empty fields
+  if (
+    [reqPhone, pin, resPhone, amount].find(
+      (field) => field.toString().trim() === ""
+    )
+  ) {
+    res.status(404).json(new ApiError(404, "Every field is required!"));
+  }
+  amount = parseInt(amount);
+
+  // Find user
+  const reqUser = await User.findOne({ mobileNumber: reqPhone });
+
+  if (!reqUser) {
+    res.status(404).json(new ApiError(404, "You don't have any account"));
+  }
+
+  const isPinValid = await reqUser.isPinCorrect(pin);
+
+  if (!isPinValid) {
+    res.status(401).json(new ApiError(401, "Invalid Credential!"));
+  }
+
+  const resUser = await User.findOne({ mobileNumber: resPhone });
+
+  // check the there have no user with this phone number
+  if (!resUser || resUser.type !== "Agent") {
+    res
+      .status(404)
+      .json(new ApiError(404, "There have no Agent with this phone number"));
+  }
+
+  // Check the user is approved or block
+  if (
+    !reqUser.isApproved ||
+    reqUser.accountStatus === "Blocked" ||
+    resUser.accountStatus === "Blocked" ||
+    !resUser.isApproved
+  ) {
+    res
+      .status(401)
+      .json(new ApiError(401, "Agent is not approved / blocked by admin"));
+  }
+
+  // calculate the fee
+  let fee = amount * 0.015;
+
+  if (reqUser.balance < amount + fee) {
+    return res
+      .status(500)
+      .json(
+        new ApiError(500, "You don't have sufficient balance to transfer!")
+      );
+  }
+
+  // Add a tranction in req user
+  const tranction = await Transaction.create({
+    reqPhone,
+    resPhone,
+    method: "cashOut",
+    amount,
+    fee,
+    isPending: true,
+  });
+
+  if (!tranction) {
+    res.status(500).json(new ApiError(500, "Transaction failed!!"));
+  }
+
+  // decrease req user balance through the amount and fees respectively after approval
+  // try {
+  //   reqUser.balance = reqUser.balance - (amount + fee);
+  //   await reqUser.save();
+  // } catch (error) {
+  //   res
+  //     .status(500)
+  //     .json(
+  //       new ApiError(500, error.message || "Can't add money to the sender")
+  //     );
+  // }
+
+  // send response
+  res
+    .status(201)
+    .send(new ApiResponse(201, tranction, "Request for cash out successfully"));
+});
+
+export { sendMoney, cashOut };
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { sendMoney } from "../controllers/transaction.controller.js";
+import { cashOut, sendMoney } from "../controllers/transaction.controller.js";
 
 const router = Router();
 
 // Secure Routes
 router.route("/send-money").post(verifyJWT, sendMoney);
+router.route("/cash-out").post(verifyJWT, cashOut);
 
 export default router;
